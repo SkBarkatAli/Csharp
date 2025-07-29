f4df1dce4f7a968f66a81bdf32ec0cee0740aab3
SkBarkatAli <skbarkatali445@gmail.com>
2025-07-29 17:25:04 +0530

    Bilt-in types in c# Boolean type, Integer types, Floating Types, Decimal Types

---
+++ BuiltInTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        /* bool b = true;  //that is ok in csharp
+
+        b = 123;    // that is not ok in csharp but it is ok in c and c++, */
+
+
+        /*int i = 0;
+
+        Console.WriteLine("Min = {0}", int.MinValue);
+        Console.WriteLine("max = {0}", int.MaxValue);*/
+
+        /*double d = 123.45458569;
+        Console.WriteLine(d);*/
+
+        /*decimal D = 19.99m;
+        Console.WriteLine(D);*/
+
+        int age = 30;
+        float pi = 3.14f;
+        decimal price = 19.99m;
+        char grade = 'A';
+        bool passed = true;
+        string name = "Alice";
+
+        Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade}, Price: {price}, Passed: {passed}");
+
+
+    }
+}
+
