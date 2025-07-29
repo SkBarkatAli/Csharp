98b89c9f9d6bdedc2be55e966277aeb7f41b079a
SkBarkatAli <skbarkatali445@gmail.com>
2025-07-29 14:46:46 +0530

    Namespace declarition and Main method is the entry point

--- introduction/Program.cs
+++ introduction/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace introduction
-{
-    internal class Program
+﻿using System;// <- namespace declaration
+class Program
     {
-        static void Main(string[] args)
-        {
-        }
+    static void Main1()
+    {
+        Console.WriteLine("Welcome to c# Trainig 1");
     }
+    static void Main() // <- Main method is the entry point into your application
+    {
+        Console.WriteLine("Welcome to c# Trainig");
+        Main1();
+    }
+
 }
+
