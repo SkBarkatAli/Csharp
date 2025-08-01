5b135ca89e99df311ecdc4b248cd34cd2b3f4f0a
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-01 18:22:48 +0530

    Comments

---
+++ Comments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+    {
+        static void Main()
+    {
+        //Console.WriteLine("hello")  Single - line Comments(//)
+
+
+        /* Console.WriteLine("hello")
+            Console.WriteLine("hello")    Multi-line Comments /* ... */
+        // Console.WriteLine("hello")     */
+
+
+
+
+        ///xml commnt learn letar
+
+
+
+
+
+
+    }
+    }
+
