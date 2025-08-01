1e89305692f17dc29ed5a04f7aff4a1cd4b8a075
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-01 11:53:08 +0530

    Nullable Types,non-nullable convert to nullable, and using null coalesing Operator

---
+++ Nullable Types/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //string name = null; //Already nullable (reference type)
+        // int i = null; // Cannot convert null to 'type' because it is a non-nullable value type
+        // int? i = null; //that makes is nullable data type.
+
+
+
+        /*  bool? AreYouMajor = null;
+          if (AreYouMajor == true)
+          {
+              Console.WriteLine("User is major");
+          }
+          else if (AreYouMajor == false)
+          {
+              Console.WriteLine("user is Not Major");
+          }
+          else
+          { Console.WriteLine("User did not answer the Question"); } */
+
+
+
+      /*  int? TicketOnSale = 0;
+        int AvailableTickers;
+        if (TicketOnSale == null)
+        {
+            AvailableTickers = 0;
+        }
+        else {
+            AvailableTickers =(int) TicketOnSale;  // the cast operator (for conversion-nullble data type to be converted into
+                                                   // non nullable data type)
+            // AvailableTickers = TicketOnSale.Value; // we can also use it
+
+        }
+        Console.WriteLine("AvailableTickets = {0}", AvailableTickers);   */
+
+
+
+        int? TicketOnSale = 0;
+
+        int AvaialableTickets = TicketOnSale ?? 0;
+
+        Console.WriteLine("AvailableTickets = {0}", AvaialableTickets);
+
+    }
+
+}
