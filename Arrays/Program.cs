604393e67254ef4436b9b4744e46dfe075f5d359
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-01 17:43:05 +0530

    using array

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+    {
+        static void Main()
+        {
+
+        int[] EvenNumber = new int[3];  //3 integer in this arry
+
+        EvenNumber[0] = 0;
+        EvenNumber[1] = 2;
+        EvenNumber[2] = 4;
+
+        Console.WriteLine(EvenNumber[1]);
+
+
+    }
+    }
+
