1f7058147c6fcfb10a0176387f49c8c2f023b21a
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-02 00:07:03 +0530

    using while loop

---
+++ While loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Please enter your target?");
+            int UserTarget = int.Parse(Console.ReadLine());
+
+        int Start = 0;                           
+
+        while (Start <= UserTarget)
+        {
+            Console.Write(Start + " ");
+            Start = Start + 2;            //Note: Dont forget to update the variable participating in the condition, so the loop can end, at some point
+        }
+        }
+    }
+
