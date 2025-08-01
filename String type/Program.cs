621117352b1be0b4766db959f7e0d3f6399491a1
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-01 07:53:09 +0530

    String type are do in first class, now i do Escape Sequence and verbatim Literal

---
+++ String type/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+    {
+        static void Main()
+        {
+        /* string Name = "\"Barkat\"";    --> \"	Double quote */
+        /* string Name = "one\nTwo\nThree";  --> \n New line */
+        /* string Name = "c:\\barkat\\DotNet\\Trining\\Csharp";  --> \\	Backslash Escape Sequences */
+        string Name = @"c:\barkat\DotNet\Trining\Csharp";  // --> verbatim Literal
+        Console.WriteLine(Name);
+        }
+    }
+
