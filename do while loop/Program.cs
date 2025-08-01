54719f7a73565f2065ef4bd35a1dac6d54a5cd8b
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-02 01:20:54 +0530

    Program coffee maker rewritten Code using do While loop

--- do while loop/Program.cs
+++ do while loop/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 1. A do loop checks its condition at the end of the loop.
 //2. This means that the do loop is guaranteed to execute at least one time.
 //3. Do loops are used to present a menu to the user
-using System;
+/*ing System;
 class Program
 {
     static void Main()
@@ -34,5 +34,56 @@ class Program
                 }
             } while (UserChoice != "YES" && UserChoice != "NO");
         } while (UserChoice == "YES");
+    }
+}
+*/
+
+
+
+//coffee maker ewritten Code Using do...while
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int TotalCoffeeCost = 0;
+        string UserDecision;
+
+        do
+        {
+            Console.WriteLine("Please select your coffee size: 1 - Small, 2 - Medium, 3 - Large");
+            int UserChoice = int.Parse(Console.ReadLine());
+
+            switch (UserChoice)
+            {
+                case 1:
+                    TotalCoffeeCost += 1;
+                    break;
+                case 2:
+                    TotalCoffeeCost += 2;
+                    break;
+                case 3:
+                    TotalCoffeeCost += 3;
+                    break;
+                default:
+                    Console.WriteLine("Your choice {0} is invalid", UserChoice);
+                    continue;  // Skip to next iteration of the loop
+            }
+
+            Console.WriteLine("Do you want to buy another coffee? (yes or no)");
+            UserDecision = Console.ReadLine();
+
+            // Keep asking for valid input if not "yes" or "no"
+            while (UserDecision.ToUpper() != "YES" && UserDecision.ToUpper() != "NO")
+            {
+                Console.WriteLine("Your choice {0} is invalid. Please enter yes or no.", UserDecision);
+                UserDecision = Console.ReadLine();
+            }
+
+        } while (UserDecision.ToUpper() == "YES");
+
+        Console.WriteLine("Thank you for shopping with us!");
+        Console.WriteLine("Bill Amount = {0}", TotalCoffeeCost);
     }
 }
