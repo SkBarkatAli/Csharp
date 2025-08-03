6410159b44033ac243558a83a6f87eb8f5fbdcb4
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-03 10:27:53 +0530

    create method and using them,instance method and static method create and use them.

---
+++ Methods/Program.cs
@@ -0,0 +1,66 @@
+﻿/*using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Program p = new Program();     // if you have to invoke an instance method you create an instance of thatclass and on the instance of that
+                                       // class you invoke this method using the dot operator
+
+        p.EvenNumbers();
+    }
+
+         public void EvenNumbers()
+    {
+        int Start = 0;
+
+        while (Start <= 20)
+        {
+            Console.WriteLine(Start);
+            Start = Start + 2;
+        }
+    }
+  }*/
+
+
+
+
+
+
+//Made this method a static method now if you look
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Program.EvenNumbers(30);
+
+        Program P = new Program();
+        int Sum = P.Add(10, 20);
+
+        Console.WriteLine("Sum = {0}",Sum);
+    }
+
+    public int Add(int FN, int SN)
+    {
+        return FN + SN;
+    }
+
+    public static void EvenNumbers(int Target)  // Print all the even number
+    //  |      |    |
+    //access modifier
+    //         |    |
+    // static modifier
+    //              |
+    //          return type
+    {
+        int Start = 0; 
+
+        while (Start <= Target)
+        {
+            Console.WriteLine(Start);
+            Start = Start + 2;
+        }
+    }
+}
+
+
