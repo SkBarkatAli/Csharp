3256b225237b6d0a618372b8b186669ed9515b00
SkBarkatAli <skbarkatali445@gmail.com>
2025-08-07 01:15:18 +0530

    Different ways to invoke ahidden base class memeber from derived class

---
+++ Method hiding/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Employee  // parent Class Implemention
+{
+    public string FirstName;
+    public string LastName;
+    
+
+
+    public void printFullName()
+    {
+      Console.WriteLine(FirstName + " " + LastName); 
+    }
+}
+
+public class FullTimeEmployee : Employee   // childClass Implementation
+{
+    public new void printFullName()  
+    {
+        //base.printFullName(); // -> for parent class used base keyword
+        Console.WriteLine(FirstName + " " + LastName + " - from child") ;
+    }
+}
+
+
+public class PartTimeEmployee : Employee
+{
+   
+}
+
+
+public class Program
+    {
+       public static void Main()
+        {
+
+        Employee FTE = new FullTimeEmployee();// a parent class reference variable can point to a child class object wherea
+        FTE.FirstName = "Barkat";
+        FTE.LastName = "Ali";
+
+        //((Employee)FTE).printFullName(); // for parent class method calld not child class.
+        FTE.printFullName();
+
+        PartTimeEmployee PTE = new PartTimeEmployee();
+        PTE.FirstName = "luci";
+        PTE.LastName = "far";
+        PTE.printFullName();
+        
+
+    }
+    }
+
+
+
